07411373e7f191b014a3a79fe55913ce136d50ee
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-07 12:44:24 -0700

    Cleaned up imports

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -14,12 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.DeveloperMode
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.Android
 import androidx.compose.material.icons.outlined.Code
-import androidx.compose.material.icons.outlined.DeveloperMode
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
