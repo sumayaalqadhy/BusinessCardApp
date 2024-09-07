fa9839e466dad13ce6b6ac6caf80890c03db4974
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-07 12:43:39 -0700

    Completed design

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.DeveloperMode
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.Android
 import androidx.compose.material.icons.outlined.Code
+import androidx.compose.material.icons.outlined.DeveloperMode
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -23,8 +28,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,7 +54,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BusinessCardApp(){
     Column(
-        Modifier.fillMaxSize()
+        Modifier
+            .fillMaxSize()
+            .background(Color(0xFFE6F2E6))
     ) {
         Introduction(
             name = "Sumaya Alqadhy",
@@ -75,20 +84,25 @@ fun Introduction(name: String, title: String, modifier: Modifier = Modifier) {
             .padding(top = 200.dp)
     ) {
         Icon(
-            imageVector = Icons.Filled.Favorite,
-            contentDescription = "fav",
-            tint = Color.Red,
-            modifier = Modifier.size(48.dp)
+            imageVector = Icons.Outlined.Android,
+            contentDescription = "Android icon",
+            tint = Color(0xFF3ddc84),
+            modifier = Modifier
+                .size(80.dp)
         )
         Text(
             text = name,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Black,
             modifier = Modifier
-
-
         )
 
         Text(
             text = title,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color(0xFF3ddc84),
             modifier = Modifier
 
         )
@@ -100,48 +114,63 @@ fun BusinessCardDetails(number: String, gitHub: String, email: String, modifier:
     Column(
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.Start,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .offset(x = 80.dp)
     ) {
 
         Row(
-            verticalAlignment = Alignment.CenterVertically
-            
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+
         ) {
             Icon(
                 imageVector = Icons.Filled.Call,
-                contentDescription = "cell"
+                contentDescription = "cell",
+                tint = Color.Black
 
             )
             Text(
                 text = number,
+                color = Color.Black,
                 modifier = Modifier
-                    .padding(start = 100.dp)
+                    .padding(start = 4.dp)
             )
         }
 
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
         ) {
             Icon(imageVector = Icons.Outlined.Code,
-                contentDescription = "github" )
+                contentDescription = "github",
+                tint = Color.Black
+            )
             Text(
                 text = gitHub,
+                color = Color.Black,
                 modifier = Modifier
-                    .padding(start = 100.dp)
+                    .padding(start = 4.dp)
             )
         }
 
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
         ) {
             Icon(
                 imageVector = Icons.Filled.Email,
-                contentDescription = "email"
+                contentDescription = "email",
+                tint = Color.Black
             )
             Text(
                 text = email,
+                color = Color.Black,
                 modifier = Modifier
-                    .padding(start = 100.dp)
+                    .padding(start = 4.dp)
             )
         }
     }
